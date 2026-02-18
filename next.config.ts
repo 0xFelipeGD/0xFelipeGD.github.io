9db13b66afec9c83ef3f55170c754bbcc53eb376
Felipe <0xfelipegd@gmail.com>
2026-02-18 15:07:16 +0000

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   /* Permite SVGs nos placeholders. Pode remover quando substituir por imagens reais. */
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
   },
 };
 
