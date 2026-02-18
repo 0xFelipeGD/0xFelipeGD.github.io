6640343cffbabb9311c8709c3a6b2325fe81ee99
Felipe <0xfelipegd@gmail.com>
2026-02-18 15:05:33 +0000

    Updates

---
+++ _next/static/o-GZOTRO1Z1xwJd9LXkox/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcareer\u002Fengineer\u002Fprojects\u002F[slug]","\u002Fcareer\u002Fwebdev\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
